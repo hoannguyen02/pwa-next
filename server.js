6800520ef6b5fef558dedc27c9fdff4f41e3c7b8
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2022-09-23 00:56:45 +0700

    Update port demo

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl);
     }
-  }).listen(3000, () => {
-    console.log(`> Ready on http://localhost:${3000}`);
+  }).listen(3001, () => {
+    console.log(`> Ready on http://localhost:${3001}`);
   });
 });
