f830bdf9009cf4321bfda45680fb6c7e04eca7ad
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2022-09-23 08:29:37 +0700

    Update manifest

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -17,6 +17,28 @@ export default function Layout({ children }) {
         />
         <link rel="apple-touch-icon" href="%PUBLIC_URL%/icon-192x192.png" />
         <link rel="manifest" href="%PUBLIC_URL%/manifest.webmanifest" />
+        <meta name="application-name" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          href="%PUBLIC_URL%/icon-192x192.png"
+        />
+        <link
+          rel="mask-icon"
+          href="%PUBLIC_URL%/icon-192x192.png"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <div
         style={{
